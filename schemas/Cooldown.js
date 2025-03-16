bde05c435a779b67dea98563fa883463f024d63e
aliem-r <ali.e.m@outlook.com>
2025-03-17 00:29:27 +0100

    Añade timestamps en el esquema de cooldown y elimina lastDailyCollect del esquema de user

--- schemas/Cooldown.js
+++ schemas/Cooldown.js
@@ -1,19 +1,22 @@
 import { Schema, model } from "mongoose";
 
-const cooldownSchema = new Schema({
-    commandName: {
-        type: String,
-        required: true,
-        unique: true,
+const cooldownSchema = new Schema(
+    {
+        commandName: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        userId: {
+            type: String,
+            required: true,
+        },
+        endsAt: {
+            type: Date,
+            required: true,
+        },
     },
-    userId: {
-        type: String,
-        required: true,
-    },
-    endsAt: {
-        type: Date,
-        required: true,
-    },
-});
+    { timestamps: true }
+);
 
 export const Cooldown = model("Cooldown", cooldownSchema);
--- schemas/User.js
+++ schemas/User.js
@@ -11,7 +11,6 @@ const userSchema = new Schema(
             type: Number,
             default: 0,
         },
-        lastDailyCollect: Date,
     },
     { timestamps: true }
 );
