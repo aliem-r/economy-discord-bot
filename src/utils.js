4b21c0cd0e95bbfbcb4ccc2ee71fd38eb8a78c4e
aliem-r <ali.e.m@outlook.com>
2025-03-16 23:42:24 +0100

    Añade funciones utilitarias que se van a ir utilizando a lo largo del proyecto

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+// Gets a random number between min and max
+export function getRandomNumber(min = 0, max = 100) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Rolls a chance based on a percentage. rollChance(60) will return true 60% of the time
+export function rollChance(percent) {
+    const randomNumber = getRandomNumber();
+    return randomNumber < percent;
+}
+
+// Formats a number as a currency
+export function formatBalance(amount) {
+    return amount.toLocaleString() + " €";
+}
